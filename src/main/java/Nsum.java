387c35aa21abb2236d22fc0e1ab96bcbfc14100b
Tomas Vasquez <vasquezt2015@gmail.com>
2024-05-08 17:59:49 -0600

    Space optimized N sum

--- src/main/java/Nsum.java
+++ src/main/java/Nsum.java
@@ -1,12 +1,35 @@
 public class Nsum {
 
+    /*
+     * Problem: Sum the first n elements.
+     * Objective Function: F(n) is the sum of the first n elements
+     * Base Case: F(0) = 0
+     * Recurrence Relation: F(n) = F(n-1) +F(n)
+     * Order of Execution: Bottom up
+     * Answer: F(n)
+     * 
+     */
+
     public static int n_sum(int n){
+        //This method is the first attempt at a solution for the n_sum problem
+        /*
+         * Runtime: O(n)
+         * Space: O(n)
+         */
         int [] sums = new int[n+1];
         sums[0] = 0;
         for(int i=1;i<=n;i++){
             sums[i] = sums[i-1] +i;
         }
         return sums[n];
+    }
+    public static int n_sum_optimized(int n){
+        //This method is optimized for size
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum= sum + i;
+        }
+        return sum;
     }
    
    
--- src/test/java/NsumTests.java
+++ src/test/java/NsumTests.java
@@ -18,5 +18,17 @@ class NsumTests {
     void nSumTestRunWithFive() {
         Assertions.assertEquals(15, Nsum.n_sum(5), "Sum for 5 should equal 15");
     }
+    @Test
+    void nSumSpaceOptimizedTestRunWithZero() {
+        assertEquals(0, Nsum.n_sum_optimized(0), "Sum for 0 should be 0");
+    }
+    @Test
+    void nSumSpaceOptimizedTestRunWithOne() {
+        assertEquals(1, Nsum.n_sum_optimized(1), "Sum for 1 should be 1");
+    }
+    @Test
+    void nSumSpaceOptimizedTestRunWithFive() {
+        Assertions.assertEquals(15, Nsum.n_sum_optimized(5), "Sum for 5 should equal 15");
+    }
     
 }
