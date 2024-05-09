94319e43f0b47f54e4fdcac30a692acaa40210ab
Tomas Vasquez <vasquezt2015@gmail.com>
2024-05-09 15:46:52 -0600

    Max profit

---
+++ src/main/java/MaxProfit.java
@@ -0,0 +1,81 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+ /*
+     * Problem: Walk through the matrix one square at a time. Return the path that maximes profit.
+     * Objective Function: F(i,j) the max profit up to the i,j step
+     * Base Case: F(i,j) = prices[0,0]
+     * Recurrence Relation: F(i,j) = max(F(i-1,j) +F(i-1,j)) + prices[i,j] // 
+     * Order of Execution: Bottom up
+     * Answer: F(n,m) for an nxm price matrix
+     * 
+     */
+    class Node{
+    int i;
+    int j;
+    int profit;
+    Node(int i, int j, int profit){
+        this.i = i;
+        this.j = j;
+        this.profit = profit;
+    }
+     String toStringNode(){
+        return String.format("Node (%d,%d) profit %d", this.i,this.j,this.profit);
+    }
+}
+public class MaxProfit {
+    public static int maxProfit(int [][] prices){
+        int[][] profits = new int[prices.length][prices[0].length];
+        ArrayList<Integer> path = new ArrayList();
+        path.add(0);
+        //ArrayList<Node> path = new ArrayList<>();
+        //Walk thro
+        //path.add(new Node(0,0,prices[0][0]));
+        //initialize profits
+        //ArrayList<Integer> list = new ArrayList<>();
+        //list.add(0);
+        //profits.add(list);
+        profits[0][0] = prices[0][0];
+        Integer previous_profit = 0;
+        for(int i=0;i<prices.length;i++){
+            for(int j=0;j<prices[0].length;j++){
+                if(j==0 && i==0){continue;}
+                int up_cell;
+                int left_cell;
+                int max_profit;
+                int new_profit = prices[i][j];
+                if(i>0 && j>0){
+                    up_cell = profits[i-1][j];
+                    left_cell = profits[i][j-1];
+                    max_profit = Math.max(up_cell,left_cell);
+                    new_profit += max_profit;
+                    
+                    
+                   
+                }else if(j>0){
+                    left_cell = profits[i][j-1];
+                    max_profit = left_cell;
+                    new_profit += max_profit ;
+
+                }else{
+                    max_profit = profits[i-1][j];
+                    new_profit += max_profit;
+                }
+                profits[i][j] = new_profit;
+                    if(max_profit != previous_profit){
+                        path.add(max_profit);
+                    }
+                
+            }
+
+        }
+        
+        // for(Node node: path){
+        //     System.out.printf( node.toStringNode()+ " ");
+        // }
+        System.out.println(path);
+        return profits[prices.length-1][prices[0].length-1];
+    }
+}
--- src/main/java/app.java
+++ src/main/java/app.java
@@ -5,10 +5,10 @@ public class app {
     public static void main(String[] args) {
         
         //int n = Integer.parseInt(args[0]);
-        Boolean [] red_stairs = {false,false,false,false,false,false,false,false};
-        int [] prices = {3,2,4,6,1,1,5,3};
-        int [] expected_path = {0,2,3,5,6,8};
-        ClimbStairs.lowestCostPathToClimbStairsKStepsSkipRedStairs(8,2, red_stairs, prices);
+        //Boolean [] red_stairs = {false,false,false,false,false,false,false,false};
+        int [][] prices = {{0,2,2,1},{3,1,1,1},{4,4,2,0}};
+        int maxprofit = MaxProfit.maxProfit(prices);
+        System.out.println(maxprofit);
     }
     
 }
